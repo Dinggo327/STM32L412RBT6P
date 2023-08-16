716d528e439c66b9f79b6b76ded36023279b5e2e
dingge0327 <435897971@DINGO>
2023-08-16 08:48:19 +0800

    TIM2 interrupt handler

---
+++ test_3.c
@@ -0,0 +1,330 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+  /* USER CODE END Header */
+  /* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "iwdg.h"
+#include "tim.h"
+#include "wwdg.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+typedef struct
+{
+	uint8_t r, g, b;
+}
+WS2812_Color;
+
+WS2812_Color ledColors[LED_COUNT];
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+void sendColorDMA(uint32_t colorBits);
+void sendResetSignal();
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+	/* USER CODE BEGIN 1 */
+
+
+	/* USER CODE END 1 */
+
+	/* MCU Configuration--------------------------------------------------------*/
+
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
+
+	/* USER CODE BEGIN Init */
+
+	/* USER CODE END Init */
+
+	/* Configure the system clock */
+	SystemClock_Config();
+
+	/* USER CODE BEGIN SysInit */
+
+	/* USER CODE END SysInit */
+
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_TIM2_Init();
+	MX_WWDG_Init();
+	MX_IWDG_Init();
+	/* USER CODE BEGIN 2 */
+	HAL_TIM_Base_Start_IT(&htim2);
+
+	HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_2);
+	uint16_t pwm = 0, mode = 1;
+	/* USER CODE END 2 */
+
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
+	while (1)
+	{
+
+		for (int i = 0; i < LED_COUNT; i++)
+		{
+			uint32_t colorBits = 0;  // 初始化一个32位的数据变量，用来构建颜色数据
+
+			for (int j = 7; j >= 0; j--)
+			{
+				uint8_t bit = (ledColors[i].g >> j) & 0x01;  //获取当前位的值（0或1），用于构建颜色数据
+				colorBits <<= 1;  //左移一位，为下一位的数据腾出位置
+				if (bit)
+				{
+					colorBits |= 0x01;
+				}// 如果当前位是1，设置colorBits的最低位为1
+			}
+
+			for (int j = 7; j >= 0; j--)
+			{
+				uint8_t bit = (ledColors[i].r >> j) & 0x01;
+				colorBits <<= 1;
+				if (bit)
+				{
+					colorBits |= 0x01;
+				}
+			}
+
+			for (int j = 7; j >= 0; j--)
+			{
+				uint8_t bit = (ledColors[i].b >> j) & 0x01;
+				colorBits <<= 1;
+				if (bit)
+				{
+					colorBits |= 0x01;
+				}
+			}
+
+			sendColorDMA(colorBits);
+		}
+
+		sendResetSignal();
+		HAL_Delay(1000);
+
+
+
+		/*HAL_GPIO_WritePin(RGB_LED_PORT, RGB_LED_PIN, GPIO_PIN_SET);
+		HAL_Delay(1000);
+
+		HAL_GPIO_WritePin(RGB_LED_PORT, RGB_LED_PIN, GPIO_PIN_RESET);
+		HAL_Delay(1000);
+
+		HAL_GPIO_WritePin(RGB_LED_PORT, RGB_LED_PIN, GPIO_PIN_SET);
+		HAL_Delay(1000);*/
+
+
+		//HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_SET);
+		//HAL_Delay(1000);
+
+		  //HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_RESET);
+		  // HAL_Delay(1000);
+
+
+	 /* USER CODE END WHILE */
+
+	 /* USER CODE BEGIN 3 */
+		 /*  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_2);
+		   //HAL_TIM_PWM_Start 函数的作用是启动定时器的 PWM 输出通道
+		   if(mode)
+		   {
+			   pwm++;
+		   }
+		   else
+		   {
+			   pwm--;
+		   }
+		   if(pwm>=500)mode=0;
+		   if(pwm==0)mode=1;
+		   HAL_Delay(2); */
+
+
+		   //IWDG
+		uint8_t cnt = 0;
+		cnt++;
+		HAL_Delay(300);
+		HAL_IWDG_Refresh(&hiwdg);  //256喂一次狗
+
+
+
+		//WWDG
+		HAL_Delay(50);
+		HAL_WWDG_Refresh(&hwwdg);
+	}
+	/* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+
+	/** Configure the main internal regulator output voltage
+	*/
+	if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+	{
+		Error_Handler();
+	}
+
+	/** Initializes the RCC Oscillators according to the specified parameters
+	* in the RCC_OscInitTypeDef structure.
+	*/
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI | RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+	RCC_OscInitStruct.LSIState = RCC_LSI_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	{
+		Error_Handler();
+	}
+
+	/** Initializes the CPU, AHB and APB buses clocks
+	*/
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+		| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSE;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+	{
+		Error_Handler();
+	}
+}
+
+/* USER CODE BEGIN 4 */
+void sendResetSignal()
+{
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_RESET);
+	HAL_Delay(RESET_DURATION_US);
+}
+void sendColorDMA(uint32_t colorBits)
+{
+	// Stop DMA and PWM before reconfiguring
+	HAL_TIM_PWM_Stop_DMA(&htim2, TIM_CHANNEL_1);
+
+	// Reconfigure the PWM parameters
+	__HAL_TIM_SET_AUTORELOAD(&htim2, colorBits);
+	__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, colorBits >> 1);
+
+	// Start DMA and PWM again
+	HAL_TIM_PWM_Start_DMA(&htim2, TIM_CHANNEL_1, &colorBits, 1);
+}
+
+
+// TIM2 interrupt handler
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef* htim)
+{
+	//htim 是 STM32Cube HAL 库中用于表示定时器（TIM - Timer）的句柄（handle）变量。
+	//这个变量是一个结构体类型的指针，包含了与特定定时器相关的配置、状态以及其他信息。在库函数中，你会经常看到通过 htim 来指定操作的是哪个定时器。
+	if (htim == &htim2) //这行代码用于检查传递给 HAL 回调函数的定时器是否为 TIM2
+	{
+		//&htim2 表示 TIM2 的句柄的地址。
+	  // This code will be executed when TIM2 period is elapsed
+	  // Perform the desired tasks here
+		  //printf("the end");
+		//HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
+
+		// Code to be executed when TIM2 period is elapsed
+			   // For example, update LED colors or other effects
+		static uint32_t colorCounter = 0;
+
+		// Update LED colors based on some pattern
+		ledColors[0].r = (colorCounter % 256);
+		ledColors[0].g = (colorCounter % 256);
+		ledColors[0].b = (colorCounter % 256);
+
+		// Send updated color using sendColorDMA function
+		sendColorDMA(ledColors[0].r | (ledColors[0].g << 8) | (ledColors[0].b << 16));
+
+		colorCounter++;
+	}
+}
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+	/* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1)
+	{
+	}
+	/* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+	/* USER CODE BEGIN 6 */
+	/* User can add his own implementation to report the file name and line number,
+	   ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+	   /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
